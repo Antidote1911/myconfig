766c9015fa96e4cc3d871aff6d5c592af3b2ab96
Antidote1911 <antidote1911@gmail.com>
2025-02-25 21:43:56 +0100

    up

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-user=`whoami`
-
 red=`tput setaf 1`
 green=`tput setaf 2`
 reset=`tput sgr0`
 
-## echo "${green}Install Xfce packages${reset}"
-## sudo pacman -S - < xfce_pkgs.txt --noconfirm
-sudo echo "$USER"
+echo "${green}Install Xfce packages${reset}"
+sudo pacman -S - < xfce_pkgs.txt --noconfirm
+sudo sed -i -e 's|Exec=geany %F|Exec=geany -i %F|g' /usr/share/applications/geany.desktop
+sudo cp -R /files/userconfig/.config /home/${USER}
+sudo cp -R /files/backgrounds/packarch /usr/share/backgrounds/packarch
+echo "${USER}"
