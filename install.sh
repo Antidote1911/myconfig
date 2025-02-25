25754fcea0cb088b5c243749a4b45ff94089aaf9
Antidote1911 <antidote1911@gmail.com>
2025-02-26 00:49:40 +0100

    up

--- install.sh
+++ install.sh
@@ -5,13 +5,18 @@ green=`tput setaf 2`
 reset=`tput sgr0`
 
 echo "${green}Install Xfce packages${reset}"
+sudo rm /etc/sudoers.d/10-installer
+sudo cp /home/${USER}/myconfig/files/02_g_wheel /etc//etc/sudoers.d/
+
 sudo pacman -S - < xfce_pkgs.txt --noconfirm
 sudo sed -i -e 's|Exec=geany %F|Exec=geany -i %F|g' /usr/share/applications/geany.desktop
 sudo systemctl enable lightdm.service
 
 sudo cp -r /home/${USER}/myconfig/files/backgrounds/packarch /usr/share/backgrounds/packarch
-sudo cp -r /home/${USER}/myconfig/files/userconfig/.config /home/${USER}/
-sudo cp -r /home/${USER}/myconfig/files/packarch-icon.png /home/${USER}/.face
+cp -r /home/${USER}/myconfig/files/userconfig/.config /home/${USER}/
+cp -r /home/${USER}/myconfig/files/packarch-icon.png /home/${USER}/.face
 sudo cp -r /home/${USER}/myconfig/files/packarch-icon.png /usr/share/pixmaps/packarch.png
+
+
 
 eos-wallpaper-set /usr/share/backgrounds/packarch/default.jpg
