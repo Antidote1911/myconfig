6d946de56124fd218c8a25259bd61013add3db74
Antidote1911 <antidote1911@gmail.com>
2025-02-25 22:20:29 +0100

    up

--- install.sh
+++ install.sh
@@ -7,6 +7,9 @@ reset=`tput sgr0`
 echo "${green}Install Xfce packages${reset}"
 sudo pacman -S - < xfce_pkgs.txt --noconfirm
 sudo sed -i -e 's|Exec=geany %F|Exec=geany -i %F|g' /usr/share/applications/geany.desktop
-sudo cp -R /files/userconfig/.config /home/${USER}
-sudo cp -R /files/backgrounds/packarch /usr/share/backgrounds/packarch
-echo "${USER}"
+sudo systemctl enable lightdm.service
+
+sudo cp -r /home/${USER}/myconfig/files/backgrounds/packarch /usr/share/backgrounds/packarch
+sudo cp -r /home/${USER}/myconfig/files/userconfig/.config /home/${USER}/
+sudo cp -r /home/${USER}/myconfig/files/packarch-icon.png /home/${USER}/.face
+sudo cp -r /home/${USER}/myconfig/files/packarch-icon.png /usr/share/pixmaps/packarch.png
