42de656ac799b577382dac9e296468f2350c9278
Antidote1911 <antidote1911@gmail.com>
2025-02-26 01:06:29 +0100

    up

--- install.sh
+++ install.sh
@@ -5,18 +5,17 @@ green=`tput setaf 2`
 reset=`tput sgr0`
 
 echo "${green}Install Xfce packages${reset}"
-sudo rm /etc/sudoers.d/10-installer
-sudo cp /home/${USER}/myconfig/files/02_g_wheel /etc//etc/sudoers.d/
+rm /etc/sudoers.d/10-installer
+cp /home/${USER}/myconfig/files/02_g_wheel /etc/sudoers.d/
+#sudo useradd -m -g users -G "adm,audio,floppy,log,network,rfkill,scanner,storage,optical,power,wheel" -s /bin/bash $username
 
-sudo pacman -S - < xfce_pkgs.txt --noconfirm
-sudo sed -i -e 's|Exec=geany %F|Exec=geany -i %F|g' /usr/share/applications/geany.desktop
-sudo systemctl enable lightdm.service
+pacman -S - < xfce_pkgs.txt --noconfirm
+sed -i -e 's|Exec=geany %F|Exec=geany -i %F|g' /usr/share/applications/geany.desktop
+systemctl enable lightdm.service
 
-sudo cp -r /home/${USER}/myconfig/files/backgrounds/packarch /usr/share/backgrounds/packarch
+cp -r /home/${USER}/myconfig/files/backgrounds/packarch /usr/share/backgrounds/packarch
 cp -r /home/${USER}/myconfig/files/userconfig/.config /home/${USER}/
 cp -r /home/${USER}/myconfig/files/packarch-icon.png /home/${USER}/.face
-sudo cp -r /home/${USER}/myconfig/files/packarch-icon.png /usr/share/pixmaps/packarch.png
-
-
+cp -r /home/${USER}/myconfig/files/packarch-icon.png /usr/share/pixmaps/packarch.png
 
 eos-wallpaper-set /usr/share/backgrounds/packarch/default.jpg
