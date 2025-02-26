9a2e245f27ab6c79c71db76a7b6916a019489de8
Antidote1911 <antidote1911@gmail.com>
2025-02-26 13:48:54 +0100

    up

--- install.sh
+++ install.sh
@@ -5,13 +5,12 @@ green=`tput setaf 2`
 reset=`tput sgr0`
 
 USER=$(whoami)
-echo "${green} Utilisateur ${whoami} ${reset}"
 
 echo "${green}Install full desktop or minimal ?${reset}"
 read -p "1 - Full, 0 - Minimal: " vm_setting
 
 sudo sed -i -e 's|%wheel ALL=(ALL:ALL) ALL|%wheel  ALL=(ALL) NOPASSWD: ALL|g' /etc/sudoers.d/10-installer 2>>log.txt
-sudo cp /home/antidote/myconfig/files/02_g_wheel /etc/sudoers.d/ 2>>log.txt
+sudo cp /home/${USER}/myconfig/files/02_g_wheel /etc/sudoers.d/ 2>>log.txt
 sudo rm /etc/sudoers.d/10-installer 2>>log.txt
 
 sudo pacman -S - < base_pkgs.txt --noconfirm 2>>log.txt
@@ -22,24 +21,24 @@ if [[ $vm_setting == 1 ]]; then
   yay -S - < extra_pkgs.txt --noconfirm 2>>log.txt
   rustup toolchain install stable 2>>log.txt
   ## Add syno nfs share to autofs
-  sudo cp -r /home/antidote/myconfig/files/auto.nfs /etc/auto.nfs 2>>log.txt
+  sudo cp -r /home/${USER}/myconfig/files/auto.nfs /etc/auto.nfs 2>>log.txt
   sudo sed -i -e 's|/misc.*|/mnt /etc/auto.nfs --ghost,--timeout=60|g' /etc/autofs/auto.master 2>>log.txt
   sudo systemctl enable autofs.service 2>>log.txt
 fi
 ####################################################
 
-sudo cp -r /home/antidote/myconfig/files/userconfig/.config /home/antidote/ 2>>log.txt
-sudo cp -r /home/antidote/myconfig/files/userconfig/.config /root/ 2>>log.txt
-sudo cp -r /home/antidote/myconfig/files/packarch-icon.png /home/antidote/.face 2>>log.txt
-sudo cp -r /home/antidote/myconfig/files/environment /etc/environment 2>>log.txt
-sudo cp -r /home/antidote/myconfig/files/userconfig/.profile /home/antidote/.profile 2>>log.txt
-sudo cp -r /home/antidote/myconfig/files/packarch-icon.png /usr/share/pixmaps/packarch.png 2>>log.txt
-sudo cp -r /home/antidote/myconfig/files/backgrounds/packarch /usr/share/backgrounds/packarch 2>>log.txt
+sudo cp -r /home/${USER}/myconfig/files/userconfig/.config /home/${USER}/ 2>>log.txt
+sudo cp -r /home/${USER}/myconfig/files/userconfig/.config /root/ 2>>log.txt
+sudo cp -r /home/${USER}/myconfig/files/packarch-icon.png /home/${USER}/.face 2>>log.txt
+sudo cp -r /home/${USER}/myconfig/files/environment /etc/environment 2>>log.txt
+sudo cp -r /home/${USER}/myconfig/files/userconfig/.profile /home/${USER}/.profile 2>>log.txt
+sudo cp -r /home/${USER}/myconfig/files/packarch-icon.png /usr/share/pixmaps/packarch.png 2>>log.txt
+sudo cp -r /home/${USER}/myconfig/files/backgrounds/packarch /usr/share/backgrounds/packarch 2>>log.txt
 
 sudo sed -i -e 's|background=/usr/share/endeavouros/backgrounds/endeavouros-wallpaper.png|background=/usr/share/backgrounds/packarch/default.jpg|g' /etc/lightdm/slick-greeter.conf 2>>log.txt
 sudo sed -i -e 's|color_scheme=nordic.conf|color_scheme=dark-colors.conf|g' /root/.config/geany/geany.conf 2>>log.txt
 sudo sed -i -e 's|Exec=geany %F|Exec=geany -i %F|g' /usr/share/applications/geany.desktop 2>>log.txt
-sudo chown -R antidote:antidote /home/antidote 2>>log.txt
+sudo chown -R ${USER}:${USER} /home/${USER} 2>>log.txt
 
 xfce4-set-wallpaper /usr/share/backgrounds/packarch/default.jpg 2>>log.txt
 sudo systemctl enable lightdm.service 2>>log.txt
