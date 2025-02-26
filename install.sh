d908c1a1fdafde1297bfe42dc843f940ecb23c7c
Antidote1911 <antidote1911@gmail.com>
2025-02-26 02:36:28 +0100

    up

--- install.sh
+++ install.sh
@@ -5,17 +5,18 @@ green=`tput setaf 2`
 reset=`tput sgr0`
 
 echo "${green}Install Xfce packages${reset}"
-rm /etc/sudoers.d/10-installer
-cp /home/antidote/myconfig/files/02_g_wheel /etc/sudoers.d/
+sudo rm /etc/sudoers.d/10-installer
+sudo cp /home/antidote/myconfig/files/02_g_wheel /etc/sudoers.d/
 
-pacman -S - < xfce_pkgs.txt --noconfirm
-sed -i -e 's|Exec=geany %F|Exec=geany -i %F|g' /usr/share/applications/geany.desktop
-systemctl enable lightdm.service
+sudo cp -r /home/antidote/myconfig/files/backgrounds/packarch /usr/share/backgrounds/packarch
+xfce4-set-wallpaper /usr/share/backgrounds/packarch/default.jpg
 
-cp -r /home/antidote/myconfig/files/backgrounds/packarch /usr/share/backgrounds/packarch
+sudo pacman -S - < xfce_pkgs.txt --noconfirm
+sudo sed -i -e 's|Exec=geany %F|Exec=geany -i %F|g' /usr/share/applications/geany.desktop
+sudo systemctl enable lightdm.service
+
+
 cp -r /home/antidote/myconfig/files/userconfig/.config /home/antidote/
 cp -r /home/antidote/myconfig/files/packarch-icon.png /home/antidote/.face
-cp -r /home/antidote/myconfig/files/packarch-icon.png /usr/share/pixmaps/packarch.png
+sudo cp -r /home/antidote/myconfig/files/packarch-icon.png /usr/share/pixmaps/packarch.png
 chown antidote:antidote /home/antidote
-
-su antidote xfce4-set-wallpaper /usr/share/backgrounds/packarch/default.jpg
